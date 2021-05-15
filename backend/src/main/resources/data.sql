4a91ba803103ebdad03ae087c7f651dfcb879256
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-15 15:57:35 -0300

    Create Course entity. Seed databasee with a course example.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,5 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2,2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3,1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3,2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3,3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.publicdomainpictures.net/pictures/270000/nahled/training-course-training-online.jpg', 'https://static.thenounproject.com/png/2036003-200.png');
