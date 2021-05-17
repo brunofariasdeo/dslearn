e5714a09769c290a5d2aa51edd9e24625931955c
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-17 08:18:20 -0300

    Create Offer entity. Link them with Many to One relation and update databasee with a new example.

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,3 +14,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3,2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3,3);
 
 INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://www.publicdomainpictures.net/pictures/270000/nahled/training-course-training-online.jpg', 'https://static.thenounproject.com/png/2036003-200.png');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-07-13T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-07-13T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-13T03:00:00Z',TIMESTAMP WITH TIME ZONE '2021-12-13T03:00:00Z', 1);
