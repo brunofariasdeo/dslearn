ee25f2f3f39fcbe317bc8365c26913640c160f56
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-05-22 18:05:11 -0300

    Add Oauth security

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO tb_user(name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user(name, email, password) VALUES ('Bob', 'bob@gmail.com', '123456');
-INSERT INTO tb_user(name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user(name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user(name, email, password) VALUES ('Bob', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user(name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
 INSERT INTO tb_role(authority) VALUES ('ROLE_STUDENT');
 INSERT INTO tb_role(authority) VALUES ('ROLE_INSTRUCTOR');
