087a3acbd82a52eded0bf7fb20639114b8c98272
Bruno Oliveira <bruno.oliveira@liferay.com>
2021-06-02 23:22:22 -0300

    Implement observer design pattern to ensure OCP solid principle

--- backend/src/main/java/com/devsuperior/dslearn/services/DeliverService.java
+++ backend/src/main/java/com/devsuperior/dslearn/services/DeliverService.java
@@ -1,11 +1,15 @@
 package com.devsuperior.dslearn.services;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslearn.dto.DeliverRevisionDTO;
 import com.devsuperior.dslearn.entities.Deliver;
+import com.devsuperior.dslearn.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearn.repositories.DeliverRepository;
 
 @Service
@@ -13,6 +17,8 @@ public class DeliverService {
 
 	@Autowired
 	private DeliverRepository repository;
+	
+	private Set<DeliverRevisionObserver> deliverRevisionObservers = new LinkedHashSet<>();
 	
 	@Transactional
 	public void saveRevision(Long id, DeliverRevisionDTO dto) {
@@ -22,5 +28,13 @@ public class DeliverService {
 		deliver.setCorrectCount(dto.getCorrectCount());
 		
 		repository.save(deliver);
+		
+		for(DeliverRevisionObserver observer : deliverRevisionObservers) {
+			observer.onSaveRevision(deliver);
+		}
+	}
+	
+	public void subscribeDeliverRevisionObserver(DeliverRevisionObserver observer) {
+		deliverRevisionObservers.add(observer);
 	}
 }
